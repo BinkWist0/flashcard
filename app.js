0a344b28e3827efca9789ae06913c787242c02af
Demid1998 <demid1998@yandex.ru>
2023-10-26 20:30:23 +0300

    next pages

--- app.js
+++ app.js
@@ -4,13 +4,31 @@ const app = express();
 const React = require("react");
 const ReactDOMServer = require("react-dom/server");
 const ThemesPage = require("./components/ThemesPage");
+const MainPage = require("./components/MainPage");
 const PORT = 3000;
+const { Theme } = require('./db/models');
 
-app.get("/themes", (req,res) => {
+app.get("/themes",  async (req,res) => {
+const thems=await Theme
+
     const reactElement = React.createElement(ThemesPage);
     const html = ReactDOMServer.renderToStaticMarkup(reactElement);
     res.send(`<!DOCTYPE html>${html}`);
 })
+
+
+app.get("/", (req,res) => {
+    const reactElement = React.createElement(MainPage);
+    const html = ReactDOMServer.renderToStaticMarkup(reactElement);
+    res.send(`<!DOCTYPE html>${html}`);
+})
+
+
+app.post('/themes',(req,res)=>{
+    
+})
+
+
 
 app.listen(PORT, () => {
   console.log("Server ready");
--- components/ThemesPage.jsx
+++ components/ThemesPage.jsx
@@ -1,20 +1,21 @@
-const React = require("react");
+const React = require('react');
 
 function ThemesPage() {
   return (
-    <div>
-      <h2>Выбери тему!</h2>
       <div>
-        <h3>Тема 1</h3>
-        <p>Машины</p>
-        <img
-          style={{ width: "200px" }}
-          src="https://rg.ru/uploads/images/187/07/30/11.jpg"
-          alt="Car"
-        />
-        <button>Выбрать</button>
+        <h2>Выбери тему!</h2>
+        <div>
+          <h3>Тема 1</h3>
+          <p>Машины</p>
+          <img
+            style={{ width: '200px' }}
+            src="https://rg.ru/uploads/images/187/07/30/11.jpg"
+            alt="Car"
+          />
+          <button>Выбрать</button>
+        </div>
       </div>
-    </div>
+   
   );
 }
 module.exports = ThemesPage;
