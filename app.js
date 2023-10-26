05b8d3ef4893e59d5a0e3ff99b2d5af32ca34d52
Ilya Ilyin <144535270+BinkWist0@users.noreply.github.com>
2023-10-26 17:54:34 +0300

    test3

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const express = require("express");
 const app = express();
 const React = require("react");
 const ReactDOMServer = require("react-dom/server");
-const MainPage = require("./components/MainPage");
+const ThemesPage = require("./components/ThemesPage");
 const PORT = 3000;
 
 app.get("/themes", (req,res) => {
-    const reactElement = React.createElement(MainPage);
+    const reactElement = React.createElement(ThemesPage);
     const html = ReactDOMServer.renderToStaticMarkup(reactElement);
     res.send(`<!DOCTYPE html>${html}`);
 })
--- components/ThemesPage.jsx
+++ components/ThemesPage.jsx
@@ -1,11 +1,12 @@
 const React = require("react");
 
-function MainPage() {
+function ThemesPage() {
   return (
     <div>
       <h2>Выбери тему!</h2>
       <div>
         <h3>Тема 1</h3>
+        <p>Машины</p>
         <img
           style={{ width: "200px" }}
           src="https://rg.ru/uploads/images/187/07/30/11.jpg"
@@ -16,4 +17,4 @@ function MainPage() {
     </div>
   );
 }
-module.exports = MainPage;
+module.exports = ThemesPage;
