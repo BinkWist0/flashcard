b3af0a1f67ba2fe529f573d1cb55bf2a7d347917
Demid1998 <demid1998@yandex.ru>
2023-10-27 10:10:12 +0300

    changesApp

--- app.js
+++ app.js
@@ -1,38 +1,91 @@
-require("@babel/register");
-const express = require("express");
+require('@babel/register');
+const express = require('express');
 const app = express();
-const React = require("react");
-const ReactDOMServer = require("react-dom/server");
-const ThemesPage = require("./components/ThemesPage");
-const MainPage = require("./components/MainPage");
+
+// настройки для сервера, чтобы при отправке формы появлялось req.body
+app.use(express.urlencoded({ extended: true }));
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+const ssr = require('./middlewares/ssr');
+const ThemesPage = require('./components/ThemesPage');
+const MainPage = require('./components/MainPage');
+const RatingPage = require('./components/RatingPage');
+const QuestionPage = require('./components/QuestionPage');
+const OurTeam = require('./components/OurTeam');
 const PORT = 3000;
 
-const { Theme } = require("./db/models");
+const { Theme } = require('./db/models');
+const { Question } = require('./db/models');
+const { Player } = require('./db/models');
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(ssr);
 
-app.get("/themes", async (req, res) => {
-  const themes = await Theme.findAll()
-  const reactElement = React.createElement(ThemesPage, {themes});
-  const html = ReactDOMServer.renderToStaticMarkup(reactElement);
-  res.send(`<!DOCTYPE html>${html}`);
+// app.get('/themes', async (req, res) => {
+//   const question = await Question.findOne({ where: { id: 1 } });
+//   // console.log(question);
+//   const themes = await Theme.findAll();
+//   const reactElement = React.createElement(ThemesPage, { themes });
+//   const html = ReactDOMServer.renderToStaticMarkup(reactElement);
+//   res.send(`<!DOCTYPE html>${html}`);
+// });
+
+app.get(`/themes/:questionid`, async (req, res) => {
+  const { questionid } = req.params;
+  // console.log(questionid, '<<<<<<');
+  const question = await Question.findOne({ where: { id: questionid } });
+  // console.log(question.quest);
+
+  // const reactElement = React.createElement(QuestionPage, { question });
+  // const html = ReactDOMServer.renderToStaticMarkup(reactElement);
+  // res.send(`<!DOCTYPE html>${html}`);
+
+  const html = res.renderComponent(QuestionPage, { question });
+  res.send(html);
 });
 
+app.get('/', (req, res) => {
+  const html = res.renderComponent(MainPage);
+  res.send(html);
 
+  // const reactElement = React.createElement(MainPage);
+  // const html = ReactDOMServer.renderToStaticMarkup(reactElement);
+  // res.send(`<!DOCTYPE html>${html}`);
+});
 
-app.get("/", (req,res) => {
-    const reactElement = React.createElement(MainPage);
-    const html = ReactDOMServer.renderToStaticMarkup(reactElement);
-    res.send(`<!DOCTYPE html>${html}`);
-})
+app.get('/team', (req, res) => {
+  const html = res.renderComponent(OurTeam);
+  res.send(html);
+});
 
+app.get('/rating', async (req, res) => {
+  const report = await Player.findAll();
+  const html = res.renderComponent(RatingPage, { report });
+  res.send(html);
 
-app.post('/themes',(req,res)=>{
-    
-})
+  // const reactElement = React.createElement(RatingPage);
+  // const html = ReactDOMServer.renderToStaticMarkup(reactElement);
+  // res.send(`<!DOCTYPE html>${html}`);
+});
 
+app.post('/themes', async (req, res) => {
+  let { namePlayer } = req.body;
+  // console.log(namePlayer, '>>>>>>>>>>>');
+  if (req.body.namePlayer === '') {
+    namePlayer = 'безымянный игрок';
+  }
+  await Player.create({ name: namePlayer });
+  const themes = await Theme.findAll();
+  // console.log(themes, '>>>>><<<>');
 
+  const html = res.renderComponent(ThemesPage, { themes, namePlayer });
+  res.send(html);
+
+  // const reactElement = React.createElement(ThemesPage, { themes, namePlayer });
+  // const html = ReactDOMServer.renderToStaticMarkup(reactElement);
+  // res.send(`<!DOCTYPE html>${html}`);
+});
 
 app.listen(PORT, () => {
-  console.log("Server ready");
+  console.log('Server ready');
 });
